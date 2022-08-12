34817f96a17af6db8f6cd0f2c07de586b1fe5b26
Alexey Sidorov <astafyeva.marina@mail.ru>
2022-08-12 18:12:14 +0400

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+	let result = [];
+	for (let i = 0; i < matrix.length; i++) {
+		if (matrix.length === 0 || i % 2 === 0) {
+			result.push(matrix[i]);
+      // length возвращает число элементов массива
+		} else if (i % 2 != 0) {
+			result.push(matrix[i].reverse());
+		}
+    //reverse обращает порядок следования элементов массива
+    //push добавляет один или более элементов в конец массива и возвращает новую длину массива
+	}
+	return result.flat();
 }
+//flat возвращает новый массив с объединёнными в него подмассивами.
